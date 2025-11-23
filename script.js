5b3811f2207db759c0171ad9538069088ed19f91
Artur Mnatsakanyan <Turafc16@gmail.com>
2025-11-23 17:55:31 +0300

    test1

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const person = ({firstName, lastName, age}) => {
-	let obj = {
-		firstName: firstName,
-		lastName: lastName,
-		age: age,
-		getFullName () {
-			return `${firstName} ${lastName}`
-		}
-	}
-	return 
-}
 
-console.log("Артур", "Мнацаканян", 25);
